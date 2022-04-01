26dad5d17cf9ee9684fb65376238cb1e33af00fe
Aluno <Aluno@DSN1251230721>
2022-04-01 16:34:11 -0300

    Heranc commit

---
+++ Heranca/src/main.java
@@ -0,0 +1,15 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		
+		Carro carro1= new Carro();
+		carro1.setAno(1900);
+		carro1.setMarca ("ford");
+		carro1.setModelo("Fiesta");
+		carro1.setQuatroPortas(true);
+
+	}
+
+}
